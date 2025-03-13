66c70abe0efca81d91e104cdcfa751107b282bc3
juanfranbrv <juanfranbrv@gmail.com>
2025-03-14 00:00:53 +0100

    Init commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Titulo de la app",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+st.header("Titulo de la app")
